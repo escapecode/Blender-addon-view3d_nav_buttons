3e3f44f6f31ecca59a39f6df1375c2e308d05920
escapecode <escape_code@yahoo.com>
2017-11-02 08:41:32 -0400

    Menu is now multi-layer, so added sub-menu system Add another menu of buttons for shortcuts (only "smooth object" there currently)

--- view3d_nav_buttons/view3d_nav_buttons.py
+++ view3d_nav_buttons/view3d_nav_buttons.py
@@ -2,12 +2,8 @@ import bpy, os
 from bpy import*
 
 class My_Props_For_3D_Dropdown(bpy.types.PropertyGroup):
-    """
-    Fake module like class
-    tp = context.window_manager.my_typeprops_3d_win
-    """
-
     display_3d_nav_buttons = bpy.props.BoolProperty(name = "Open/Close", description = "Open/Close", default = False)
+    display_shortcuts = bpy.props.BoolProperty(name = "Open/Close", description = "Open/Close", default = False)
 
 bpy.utils.register_class(My_Props_For_3D_Dropdown)
 bpy.types.WindowManager.my_typeprops_3d_win = bpy.props.PointerProperty(type = My_Props_For_3D_Dropdown)
@@ -26,7 +22,7 @@ class CameraLockToViewb(bpy.types.Operator):
         return {'FINISHED'}            # this lets blender know the operator finished successfully.
 
 
-class Button_Activator(bpy.types.Header):
+class ButtonHandler(bpy.types.Header):
     bl_space_type = 'VIEW_3D'
 
     @classmethod
@@ -35,38 +31,49 @@ class Button_Activator(bpy.types.Header):
 
 
     def draw(self, context):
-        tp = context.window_manager.my_typeprops_3d_win
+        my_type_props = context.window_manager.my_typeprops_3d_win
         layout = self.layout
 
         icons = icon_collections["main"]
 
-        if tp.display_3d_nav_buttons:
 
+        if my_type_props.display_3d_nav_buttons:
             row = layout.row(1)
-
-            row.operator("view3d.viewnumpad",  text="", icon_value= icons.get("top").icon_id).type = "TOP"; # NOTE: bpy.ops.view3d.viewnumpad
-            row.operator("view3d.viewnumpad", text="", icon_value= icons.get("front").icon_id).type = "FRONT";
-            row.operator("view3d.viewnumpad", text="", icon_value= icons.get("right").icon_id).type = "RIGHT";
-            row.operator("view3d.viewnumpad", text="", icon_value= icons.get("camera").icon_id).type = "CAMERA";
-            row.operator("view3d.camera_to_view", text="", icon_value= icons.get("cam2view").icon_id);
+            row.operator("view3d.viewnumpad",  text="", icon_value= icons.get("top").icon_id).type = "TOP";# NOTE: bpy.ops.view3d.viewnumpad
+            row.operator("view3d.viewnumpad", text="", icon_value= icons.get("front").icon_id).type = "FRONT"
+            row.operator("view3d.viewnumpad", text="", icon_value= icons.get("right").icon_id).type = "RIGHT"
+            row.operator("view3d.viewnumpad", text="", icon_value= icons.get("camera").icon_id).type = "CAMERA"
+            row.operator("view3d.camera_to_view", text="", icon_value= icons.get("cam2view").icon_id)
             row.operator(CameraLockToViewb.bl_idname, text="", icon_value= icons.get("lock").icon_id)
 
+        if my_type_props.display_shortcuts:
             row = layout.row(1)
+            row.operator("object.shade_smooth",  text="", icon_value= icons.get("smooth").icon_id) # NOTE: bpy.ops.view3d.viewnumpad
 
-def menu_items_add(self, context):
-    my_type_props = context.window_manager.my_typeprops_3d_win
+class MenuHandler(bpy.types.Menu):
+    bl_label = 'Useful 3D Menu Buttons'
+    bl_idname = 'view3d.buttonmenu_handler'
+
+    def draw(self, context):
+        my_type_props = context.window_manager.my_typeprops_3d_win
+        layout = self.layout
+
+        if my_type_props.display_shortcuts:
+            layout.prop(my_type_props, "display_shortcuts", text="Buttons Shortcut")
+        else:
+            layout.prop(my_type_props, "display_shortcuts", text="Buttons Shortcut")
+
+        if my_type_props.display_3d_nav_buttons:
+            layout.prop(my_type_props, "display_3d_nav_buttons", text="Buttons Navigation")
+        else:
+            layout.prop(my_type_props, "display_3d_nav_buttons", text="Buttons Navigation")
+
+def menu_buttons_toplevel(self, context):
     layout = self.layout
 
     layout.separator()
-
-    # layout.operator("view3d.enable_all", text="Enable All",  icon = "TRIA_RIGHT")
-    # layout.operator("view3d.disable_all", text="Disable All",  icon = "TRIA_LEFT")
-
-    if my_type_props.display_3d_nav_buttons:
-        layout.prop(my_type_props, "display_3d_nav_buttons", text="Navigation Buttons")
-    else:
-        layout.prop(my_type_props, "display_3d_nav_buttons", text="Navigation Buttons")
-    layout.separator()
+    layout.menu(MenuHandler.bl_idname)
+    layout.operator("render.render")
 
 
 ### Registry ###
@@ -83,13 +90,13 @@ def register():
     view3d_nav_buttons.load("camera", os.path.join(icons_dir, "camera.png"), 'IMAGE')
     view3d_nav_buttons.load("cam2view", os.path.join(icons_dir, "cam2view.png"), 'IMAGE')
     view3d_nav_buttons.load("lock", os.path.join(icons_dir, "lock.png"), 'IMAGE')
-
-
+    view3d_nav_buttons.load("smooth", os.path.join(icons_dir, "smooth.png"), 'IMAGE')
     icon_collections['main'] = view3d_nav_buttons
 
-    bpy.utils.register_class(Button_Activator)
+    bpy.utils.register_class(ButtonHandler)
+    bpy.utils.register_class(MenuHandler)
     bpy.utils.register_class(CameraLockToViewb)
-    bpy.types.VIEW3D_MT_view.prepend(menu_items_add)
+    bpy.types.VIEW3D_MT_view.prepend(menu_buttons_toplevel)
 
 
 def unregister():
@@ -98,9 +105,9 @@ def unregister():
         bpy.utils.previews.remove(icon)
     icon_collections.clear()
 
-    bpy.utils.unregister_class(Button_Activator)
+    bpy.utils.unregister_class(ButtonHandler)
     bpy.utils.unregister_class(CameraLockToViewb)
-    bpy.types.VIEW3D_MT_view.remove(menu_items_add)
+    bpy.types.VIEW3D_MT_view.remove(menu_buttons_toplevel)
 
 if __name__ == "__main__":
     register()
